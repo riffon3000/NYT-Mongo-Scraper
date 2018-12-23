8e83f29c6b057758d9b84d57d73ee27eefae8944
riffon3000 <riffon@gmail.com>
2018-12-23 12:23:31 -0600

    handlebars

--- server.js
+++ server.js
@@ -12,12 +12,22 @@ const db = require("./models");
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+// Make public a static folder
+app.use(express.static("public"));
+
 // Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Make public a static folder
-app.use(express.static("public"));
+// Set Handlebars.
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+const routes = require("./controllers/controller.js");
+app.use(routes);
 
 // Connect to the Mongo DB. If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
